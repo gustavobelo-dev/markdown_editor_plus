3808e464d3f3807a25b49335977bba3f9dbfb905
Gustavo Belo <ogustavobelo@gmail.com>
2026-01-09 16:14:19 -0300

    replaces library to flutter_markdown_plus

--- lib/widgets/splitted_markdown_form_field.dart
+++ lib/widgets/splitted_markdown_form_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:markdown_editor_plus/src/toolbar.dart';
 import '../src/constants.dart';
 import '../src/emoji_input_formatter.dart';
